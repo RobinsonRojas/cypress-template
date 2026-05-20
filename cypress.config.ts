daccdaad34bcd1c7b3f2b5da500f8532eaf2bc46
Robinson Arley Rojas Novoa <1robinsonrojas@gmail.com>
2026-05-20 15:26:24 -0500

    feat: update .gitignore and cypress.config.ts for improved reporting and environment handling

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,46 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
+import fs from 'fs';
 
 export default defineConfig({
   allowCypressEnv: false,
-
+  retries: 2,
+  projectId: '',
+  video: true,
+  videosFolder: 'cypress/reports/videos',
+  screenshotsFolder: 'cypress/reports/screenshots',
+  downloadsFolder: 'cypress/reports/downloads',
+  reporter: 'junit',
+  reporterOptions: {
+    mochaFile: 'cypress/reports/junit/test-results-[hash].xml'
+  },
   e2e: {
+    requestTimeout: 10000,
+    responseTimeout: 60000,
+    pageLoadTimeout: 120000,
+    defaultCommandTimeout: 15000,
+    baseUrl: 'https://example.cypress.io',
+    experimentalModifyObstructiveThirdPartyCode: true,
+    experimentalRunAllSpecs: true,
+
+    env: {
+      username: process.env.USERNAME,
+      password: process.env.PASSWORD
+    },
+
     setupNodeEvents(on, config) {
       // implement node event listeners here
-    },
-  },
+      on('after:spec', (spec: Cypress.Spec, results: CypressCommandLine.RunResult) => {
+        if (results && results.video) {
+          // Do we have failures for any retry attempts?
+          const failures = results.tests.some((test) =>
+            test.attempts.some((attempt) => attempt.state === 'failed')
+          );
+          if (!failures) {
+            // delete the video if the spec passed and no tests retried
+            fs.unlinkSync(results.video);
+          }
+        }
+      });
+    }
+  }
 });
